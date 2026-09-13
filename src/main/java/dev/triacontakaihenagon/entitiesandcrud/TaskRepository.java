bc1bf8ad66af27c27fb17d7caedcc4364f14c339
31gon <327284999+31gon@users.noreply.github.com>
2026-09-14 00:48:25 +0300

    add TaskService
    Sound like hard, but it doesn't. Also turns out JRA merge Insert and Update into save, what is makes sence

--- src/main/java/dev/triacontakaihenagon/entitiesandcrud/TaskRepository.java
+++ src/main/java/dev/triacontakaihenagon/entitiesandcrud/TaskRepository.java
@@ -2,5 +2,9 @@ package dev.triacontakaihenagon.entitiesandcrud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findTaskByDoneTrue();
+
 }
--- src/main/java/dev/triacontakaihenagon/entitiesandcrud/TaskService.java
+++ src/main/java/dev/triacontakaihenagon/entitiesandcrud/TaskService.java
@@ -1,4 +1,30 @@
 package dev.triacontakaihenagon.entitiesandcrud;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+    public List<Task> getAllTask() {
+        return taskRepository.findAll();
+    }
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
