be32f9f3e75abb88b00e72d2e9c606d7b1943023
31gon <327284999+31gon@users.noreply.github.com>
2026-09-13 03:45:43 +0300

    init commit
    This project just to learn how, so nothing important you may find here. Or at least i think so

---
+++ src/main/java/dev/triacontakaihenagon/entitiesandcrud/EntitiesAndCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.triacontakaihenagon.entitiesandcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitiesAndCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitiesAndCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/triacontakaihenagon/entitiesandcrud/EntitiesAndCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.triacontakaihenagon.entitiesandcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntitiesAndCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
