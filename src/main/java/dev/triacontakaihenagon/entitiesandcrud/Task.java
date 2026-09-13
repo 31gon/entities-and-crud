4abd6427d65d716e41cceb7e92a5dedd7c9183dc
31gon <327284999+31gon@users.noreply.github.com>
2026-09-13 05:15:52 +0300

    First enity
    I think lombok actualy nice. First mine interaction with it was terrible because it was broken dependency in project. Now i see why it such a nice dependency. Also creating a DB entity interesting part. I want to say that i need to learn other option i see here, there a lot of stuff for first night

---
+++ src/main/java/dev/triacontakaihenagon/entitiesandcrud/Task.java
@@ -0,0 +1,28 @@
+package dev.triacontakaihenagon.entitiesandcrud;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    public Task() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean done;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
